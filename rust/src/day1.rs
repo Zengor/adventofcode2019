7cca387cedff4bdc864ebbe774d9350336cd5fa0
Zengor <ialmeida@edu.unifor.br>
2019-12-01 15:14:50 +0900

    Add day 1 rust solution

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,2 +1,30 @@
-pub fn test() {
+fn transform(x: i32) -> i32 {
+    x / 3 - 2
+}
+
+pub fn part1(input: &str) -> i32 {      
+    input
+        .lines()
+        .map(|l| l.parse().expect("failed parsing line"))
+        .map(transform)
+        .sum()
+}
+
+
+fn continuous_transform(mut x: i32) -> i32 {
+    let mut sum = 0;
+    x = transform(x);
+    while x > 0 {
+        sum += x;
+        x = transform(x);
+    }
+    sum
+}
+
+pub fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|l| l.parse().expect("failed parsing line"))
+        .map(continuous_transform)
+        .sum()
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,8 @@
 use adventofcode2019::*;
 
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../../input/01-1.txt");
+    let result1 = day1::part1(input);
+    let result2 = day1::part2(input);
+    println!("part 1 {} part 2 {}", result1, result2);
 }
