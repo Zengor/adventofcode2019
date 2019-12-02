49f5cd44ce1e65770217a43b405697d782003f44
Zengor <ialmeida@edu.unifor.br>
2019-12-02 18:18:12 +0900

    Add day 2 rust solution

---
+++ rust/src/day2.rs
@@ -0,0 +1,84 @@
+#[derive(PartialEq, Eq)]
+enum Intcode {
+    Add,
+    Mul,
+    Halt,
+    // this should end never being used in a well-formed program
+    // as the program cursor will only ever really convert opcode positions
+    Value(usize),
+}
+
+impl From<usize> for Intcode {
+    fn from(code: usize) -> Self {
+        use Intcode::*;
+        match code {
+            1 => Add,
+            2 => Mul,
+            99 => Halt,
+            x => Value(x),
+        }
+    }
+}
+
+fn get_operands(cursor: usize, codes: &[usize]) -> (usize, usize, usize) {
+    (codes[cursor + 1], codes[cursor + 2], codes[cursor + 3])
+}
+
+fn operate(cursor: usize, function: impl Fn(usize, usize) -> usize, codes: &mut [usize]) {
+    let (op1, op2, result_pos) = get_operands(cursor, codes);
+    codes[result_pos] = function(codes[op1], codes[op2]);
+}
+
+fn run_program(codes: &mut [usize]) {
+    let mut cursor = 0;
+    let mut next_instruction: Intcode = codes[cursor].into();
+    while next_instruction != Intcode::Halt {
+        let function = match next_instruction {
+            Intcode::Add => std::ops::Add::add,
+            Intcode::Mul => std::ops::Mul::mul,
+            _ => unreachable!("This shouldn't happen"),
+        };
+        operate(cursor, function, codes);
+        cursor += 4;
+        next_instruction = codes[cursor].into();
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    // as the codes are used for indexing and can never be negative, i use usize
+    // instead of a signed integer type
+    let mut codes: Vec<usize> = input
+        .trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    codes[1] = 12;
+    codes[2] = 2;
+    run_program(&mut codes);
+    codes[0]
+}
+
+pub fn try_inputs(noun: usize, verb: usize, codes: &[usize]) -> usize {
+    let mut codes = codes.to_owned();
+    codes[1] = noun;
+    codes[2] = verb;
+    run_program(&mut codes);
+    codes[0]
+}
+
+pub fn part2(input: &str) -> usize {
+    use itertools::iproduct;
+    let desired_output = 19690720;
+    let mut codes: Vec<usize> = input
+        .trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    for (noun, verb) in iproduct!((0..=99), (0..=99)) {        
+        let result = try_inputs(noun, verb, &codes);
+        if result == desired_output {                       
+            return 100 * noun + verb
+        }
+    }
+    unreachable!("It's assumed the puzzle will have _a_ valid solution");    
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,8 @@
 use adventofcode2019::*;
 
 fn main() {
-    let input = include_str!("../../input/01-1.txt");
-    let result1 = day1::part1(input);
-    let result2 = day1::part2(input);
+    let input = include_str!("../../input/02-1.txt");
+    let result1 = day2::part1(input);
+    let result2 = day2::part2(input);
     println!("part 1 {} part 2 {}", result1, result2);
 }
