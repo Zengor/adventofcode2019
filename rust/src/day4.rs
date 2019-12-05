172ca3171ede83819b4afd1ddb519f76dfd79738
Zengor <ialmeida@edu.unifor.br>
2019-12-05 23:56:00 +0900

    Make slight changes to day 4 rust

--- rust/src/day4.rs
+++ rust/src/day4.rs
@@ -1,13 +1,14 @@
+use itertools::Itertools;
 use std::cmp::Ordering;
 
 const DIGIT_OFFSETS: &'static [u32] = &[1, 10, 100, 1000, 10000, 100000];
 
-fn get_digit(num: u32, digit: usize) -> u32 {
-    (num / DIGIT_OFFSETS[digit]) % 10
+fn get_digits(num: u32) -> impl Iterator<Item=u32> {
+    (0..5).rev().map(move |i| (num / DIGIT_OFFSETS[i]) % 10)
 }
 
 fn valid_password(num: u32) -> (bool, bool) {
-    let mut digits = (0..=5).rev().map(|i| get_digit(num, i));
+    let mut digits = get_digits(num);
     let mut prev = digits.next().unwrap();
     let mut counts = vec![1];
     for curr in digits {
@@ -20,13 +21,17 @@ fn valid_password(num: u32) -> (bool, bool) {
     }
     counts.into_iter().fold((false, false), |(v_p1, v_p2), x| {
         ((v_p1 || x >= 2), (v_p2 || x == 2))
-    })        
+    })
 }
 
 pub fn part1_part2(input: &str) -> (u32, u32) {
-    let (mut p1_count, mut p2_count) = (0,0);
-    let mut input = input.trim().split("-").map(|i| i.parse().unwrap());
-    let (low, high) = (input.next().unwrap(), input.next().unwrap());
+    let (mut p1_count, mut p2_count) = (0, 0);
+    let (low, high) = input
+        .trim()
+        .split("-")
+        .map(|i| i.parse().unwrap())
+        .collect_tuple()
+        .unwrap();
     for value in low..=high {
         let (p1, p2) = valid_password(value);
         if p1 {
@@ -36,6 +41,5 @@ pub fn part1_part2(input: &str) -> (u32, u32) {
             p2_count += 1
         }
     }
-    
-    (p1_count,p2_count)
+    (p1_count, p2_count)
 }
